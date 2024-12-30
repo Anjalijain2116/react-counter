6f7f0de5c18febb9313af81b33bede3336a0c853
Anjali jain <anjalijain6010@gmail.com>
2024-12-31 01:07:41 +0530

    has router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import Counter from "./components/Counter.tsx";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { Route, Routes, HashRouter } from "react-router-dom";
 import CountersPage from "./pages/counterPage.tsx";
 import "./App.css";
 import Task3 from "./pages/task3.jsx";
@@ -8,16 +7,15 @@ import Home from "./pages/home.jsx";
 
 function App() {
   return (
-    <Router>
-    <div className="App">
-     
-      <Routes>
-        <Route path="/" element={<Home/>} />
+    <HashRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/counters" element={<CountersPage />} />
           <Route path="/task3" element={<Task3 />} />
         </Routes>
-    </div>
-    </Router>
+      </div>
+    </HashRouter>
   );
 }
 
